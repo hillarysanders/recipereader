e3e3a4606ea2d1c9688569f8abfddf79dc4defe5
Hillary Sanders <hillary@premise.com>
2016-08-31 12:06:05 -0700

    changed time zone

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -103,15 +103,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'US/Pacific'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
