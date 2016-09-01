b82e05ce6acd8bd47f1602efdf5c1a74f95bd227
Hillary Sanders <hillary@premise.com>
2016-09-01 09:58:42 -0700

    added more admin site functionality, searching. Finished with base tutorial site. Wut wuuuut!

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
-from .models import Question
-
+from .models import Choice, Question
 # Register your models here.
 
+
+# note the class that ChoiceInline inherits from. admin.TabularInline makes a more succinct,
+# tabular format, while admin.StackedInline makes a stacked format.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    # by default offer two choices:
+    extra = 2
+
+
 # We need to tell the admin that Question objects should have an admin interface:
-admin.site.register(Question)
+# this class isn't required, it just allows you to order your fields in the admin site interface:
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    # This tells Django: "Choice objects are edited on the Question admin page"
+    inlines = [ChoiceInline]
+    # this tells django what to show when describing a question object (optional, default is just __str__):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # this adds a search box to the top of the admin/polls/question page, so you can search for question text:
+    search_fields = ['question_text']
+
+
+
+# this does the registering:
+admin.site.register(Question, QuestionAdmin)
+
