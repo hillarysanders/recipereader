3d044211d7d9ebbb7db562271b686bb2d5ff87c0
Hillary Sanders <hillary@premise.com>
2016-08-31 21:07:53 -0700

    added results pages, poll forms pages.

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
-from .models import Question
+from django.urls import reverse
+from .models import Question, Choice
 # Create your views here.
 
 
@@ -21,10 +22,32 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
+
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.POST is a dictionary like object that lets you access submitted data by key name
+        # In this case, request.POST['choice'] returns the id of the selected choice, always as a string.
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        # ^^ this reverse() function will return a string like: '/polls/3/results/'.
+        # reverse() is given the name of the view that we want to pass control to and the variable portion of the
+        # URL pattern that points to that view, based on polls/urls.py.results.
+
