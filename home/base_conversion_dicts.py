39ff3f5b19083990587a117024d0865c59426c76
Hillary Sanders <hillary@premise.com>
2016-09-14 23:46:32 -0700

    semi major conversion revision. Better capabilities

--- home/base_conversion_dicts.py
+++ home/base_conversion_dicts.py
@@ -32,30 +32,6 @@ name_maps_numbers = [
          value=1.)
 ]
 
-# name_maps_numbers = [
-#     # don't want to match to... half and half, half chopped, etc
-#     dict(pattern=['half', 'a half', 'one half', '1/2', '\.5', 'halves'],
-#          name='1/2',
-#          value=.5),
-#     dict(pattern=['a quarter', 'one quarter', '1/4', '\.25', 'a forth', 'quarters'],
-#          name='1/4',
-#          value=.25),
-#     dict(pattern=['a fifth', 'one fifth', '1/5', '\.2', 'a fifth', 'fifths'],
-#          name='1/5',
-#          value=.2),
-#     dict(pattern=['one third', 'a third', '1/3', 'thirds'],
-#          name='1/3',
-#          value=float(1) / 3),
-#     dict(pattern=['one sixth', 'a sixth', '1/6', 'sixths'],
-#          name='1/6',
-#          value=float(1) / 6),
-#     dict(pattern=['one eighth', 'an eighth', '1/8', 'eighths'],
-#          name='1/8',
-#          value=float(1) / 8),
-#     dict(pattern=['the whole', 'a whole', 'one'],  # todo protect against e.g. 'one at at time'...
-#          name='1',
-#          value=1.)
-# ]
 # anything above 1 uses num_to_words
 name_maps_volume = [
     dict(pattern=['teaspoon', 'tsp', 't'],
@@ -116,8 +92,8 @@ def _prep_name_map(name_maps):
     """
     # capitalize stuff:
     name_maps = _extend_pattern_with_capitalization(name_maps)
-    # add spaces:
-    name_maps = _add_spaces_to_sides(name_maps)
+    # # add spaces:
+    # name_maps = _add_spaces_to_sides(name_maps)
     # coerce to dataframe:
     name_maps = pd.concat([pd.DataFrame(d) for d in name_maps])
     name_maps = name_maps.drop_duplicates('pattern')
@@ -171,6 +147,10 @@ def _extend_number_name_maps(name_maps_numbers, n=1000):
 name_maps_numbers = _extend_number_name_maps(name_maps_numbers, n=100)
 # capitalize stuff:
 name_maps_numbers = _prep_name_map(name_maps_numbers)
+name_maps_numbers['type'] = 'number'
 name_maps_volume = _prep_name_map(name_maps_volume)
+name_maps_volume['type'] = 'volume'
 name_maps_weight = _prep_name_map(name_maps_weight)
+name_maps_weight['type'] = 'weight'
 
+name_maps = pd.concat([name_maps_numbers, name_maps_volume, name_maps_weight])
--- home/conversions.py
+++ home/conversions.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 import re
 import pandas as pd
-from .base_conversion_dicts import name_maps_numbers, name_maps_volume, name_maps_weight
+from .base_conversion_dicts import name_maps_numbers, name_maps_volume, name_maps_weight, name_maps
 
 
 def multiple_replace(pattern_replace_dict, text):
@@ -54,34 +54,154 @@ def find_number_matches(line, name_maps):
     return out
 
 
-def parse_ingredient_line(line='2 and a half egg yolks, whisked'):
+def clean_newlines(x):
+    return x.replace('\r\n', '\n').replace('\r', '\n')
+
+
+X = """2 1/4 cups all-purpose flour
+1 teaspoon baking soda
+1 teaspoon salt
+1 cup (2 sticks) butter, softened
+3/4 cup granulated sugar
+3/4 cup packed brown sugar
+1 teaspoon vanilla extract
+2 large eggs
+2 cups (12-oz. pkg.) NESTLÉ® TOLL HOUSE® Semi-Sweet Chocolate Morsels
+1 cup chopped nuts"""
+
+
+def parse_ingredient_line(line):
+    # todo add in patterns and treatment for floats, e.g. 2.5 pounds.
+    patterns = ['[ \(]{}s?[\) $]|^{} | {}$'.format(p,p,p) for p in name_maps.index]
+    pattern = '|'.join(reversed(patterns))
+    # pattern = re.compile(pattern)
 
     original_line = line
-    line = clean_line(line)
-    matches = find_number_matches(line=line, name_maps=name_maps_numbers)
-    # do the same for volume and units but determine plurality based on nearest number(s) to the left?
+    match_info = pd.DataFrame()
+    placement = 0
+    while len(line) > 0:
+        match = re.search(pattern=pattern, string=line)
+        if match:
+            start = match.start()
+            end = match.end()
+            p = match.group()
+            if re.search('[) ]$', p):
+                p = p[:-1]
+                end -= 1
+            if re.search('^[( ]', p):
+                p = p[1:]
+                start += 1
 
-    if len(matches) > 0:
-        line = multiple_replace(pattern_replace_dict={r.pattern: r.replacement for
-                                                      p, r in matches.iterrows()},
-                                text=line)
+            # todo raise warning if p is not in the index of name_maps
+            pidx = p if p in name_maps.index else p[:-1]
+            info = pd.DataFrame(name_maps.loc[pidx, :]).T
+            info['original'] = p
+            info['start'] = start + placement
+            info['end'] = end + placement
+            info['pattern'] = pidx
 
-    line = line.replace(' _(_ ', '(').replace(' _)_ ', ')')
-    line = re.sub('^ | $', '', line)
+            # handle unit plurality
+            if info.loc[pidx, 'type'] in ['volume', 'weight']:
+                # was the last number != 1?
+                is_plural = 'unknown'
+                if len(match_info)>0:
+                    if any(match_info.type == 'number'):
+                        m = match_info.loc[match_info.type == 'number', 'value']
+                        if len(m)>1:
+                            m = m.values[-1]
+                        if m == 1:
+                            is_plural = False
+                            info['value'] = info['singular']
+                        else:
+                            is_plural = True
+                            info['value'] = info['plural']
+                        info['is_plural'] = is_plural
+                # if that didn't work:
+                if is_plural not in [True, False]:
+                    # todo raise warning that plurality could not be found...
+                    info['value'] = info['original']
 
-    # coerce matches df to dict:
-    matches.index = [str(i) for i in matches.index]
-    matches = matches.to_dict(orient='index')
+            placement = end
+            match_info = pd.concat([match_info, info])
+        else:
+            end = len(line)
 
-    return dict(original_line=original_line,
-                parsed_line=line,
-                matches=matches)
+        line = line[end:]
+
+    # todo now add rows for all the text in between the match starts and end, and then sort the dataframe by start! <3.
+    # todo now add rows for all the text in between the match starts and end, and then sort the dataframe by start! <3.
+    # todo now add rows for all the text in between the match starts and end, and then sort the dataframe by start! <3.
+    # todo how to handle floats? Have to handle number parsing and periods somehow.... Maybe if type=='number' look to
+    # todo    the left, if it's a period, then combine that and multiple value by .01. If not, then look to the right.
+    # todo    If it's a period or dash and then a number, combine the two by addition....
+    # todo    hm. OR. Add a '[0-9]*\.?[0-9]* type pattern to the start of the pattern above and just add a section
+    # todo    that says, oh, this is a float and not in the index, so just do (float(trim(p)). Hm. That might be best.
+    # todo    probably faster.
+
+    return match_info
 
 
-def parse_ingredients(lines):
-    ll = lines.split('\n')
-    results_dict = {str(i): parse_ingredient_line(ll[i]) for i in range(len(ll))}
+def parse_ingredients(x):
+    x = clean_newlines(x)
+    lines = x.split('\n')
+    results_dict = {str(i): parse_ingredient_line(lines[i]) for i in range(len(lines))}
     return results_dict
 
 
+def get_highlighted_ingredients(recipe, prefix='<highlighted>', postfix='</highlighted>'):
+    ing = recipe.ingredients
+    idx = sorted(ing.keys())
+    highlighted = []
+    for i in idx:
+        line = ing[i]['parsed_line']
+        x = parse_ingredient_line(line)
+        matches = x['matches']
 
+        positions = sorted([[m['start'], m['end']] for m in matches.values()])
+        h = ''
+        end_ = 0
+        for se in positions:
+            start = se[0]
+            end = se[1]
+            h += line[end_:start]+prefix
+            h += line[start:end]+postfix
+            end_ = end
+        h += line[end:len(line)]
+        highlighted.append(h)
+
+        # todo!! highlights ARE working (yay!) but the placement is off because the match positions refer to the line
+        # todo   with e.g. '_(_' instead of '(' values, etc. I think ideally we want the output dict to contain
+        # todo   start and end values of the final line, not the 'orginal' (actually, the preformatted) original line.
+        # Probably end up redesigning the match dict stored object a bit.
+
+    return highlighted
+
+
+# def parse_ingredient_line(line='2 and a half egg yolks, whisked'):
+#
+#     original_line = line
+#     line = clean_line(line)
+#     matches = find_number_matches(line=line, name_maps=name_maps_numbers)
+#     # do the same for volume and units but determine plurality based on nearest number(s) to the left?
+#
+#     if len(matches) > 0:
+#         line = multiple_replace(pattern_replace_dict={r.pattern: r.replacement for
+#                                                       p, r in matches.iterrows()},
+#                                 text=line)
+#
+#     line = line.replace(' _(_ ', '(').replace(' _)_ ', ')')
+#     line = re.sub('^ | $', '', line)
+#
+#     # coerce matches df to dict:
+#     matches.index = [str(i) for i in matches.index]
+#     matches = matches.to_dict(orient='index')
+#
+#     return dict(original_line=original_line,
+#                 parsed_line=line,
+#                 matches=matches)
+#
+#
+# def parse_ingredients(lines):
+#     ll = lines.split('\n')
+#     results_dict = {str(i): parse_ingredient_line(ll[i]) for i in range(len(ll))}
+#     return results_dict
