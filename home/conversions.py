a6919b8a60ed123c31276fd513de0442baa5dc4d
Hillary Sanders <hillary@premise.com>
2016-09-18 14:05:37 -0700

    added text parsing for integers, float, fractions

--- home/conversions.py
+++ home/conversions.py
@@ -41,13 +41,17 @@ def parse_ingredient_line(line):
     # sooo... first look for numbers. Then loop through the rest of the text using this code?
     patterns = ['[ \(]{}s?[\) $]|^{} | {}$'.format(p, p, p) for p in name_maps.index]
     pattern = '|'.join(reversed(patterns))
+    # prepend this pattern with a pattern for integers, floats, and simple fractions:
+    float_pat = '\.?\d/\.?\d|\d+\.?\d+|\d+|\.\d+'
+    pattern = '|'.join([float_pat, pattern])
+    pattern = re.compile(pattern, re.IGNORECASE)
     # pattern = re.compile(pattern)
 
     original_line = line
     match_info = pd.DataFrame()
     placement = 0
     while len(line) > 0:
-        match = re.search(pattern=pattern, string=line)
+        match = pattern.search(line)
         if match:
             start = match.start()
             end = match.end()
@@ -60,42 +64,56 @@ def parse_ingredient_line(line):
                 p = p[1:]
                 start += 1
 
-            # if p isn't in the index of name_maps, it probably has an s on the end:
-            pidx = p if p in name_maps.index else p[:-1]
-            # we want the start and end to stay the same but the pattern to forget the s.
-            # todo raise warning if p is still not in the index of name_maps
+            # if it's a float pattern match, we build the row by hand
+            if re.search(float_pat, p):
+                if '/' in p:
+                    value = p.split('/')
+                    value = float(value[0]) / float(value[1])
+                else:
+                    value = float(p)
 
-            info = pd.DataFrame(name_maps.loc[pidx, :]).T
-            info['original'] = p
-            info['start'] = start + placement
-            info['end'] = end + placement
-            info['pattern'] = pidx
+                info = pd.DataFrame(dict(start=start + placement, end=end + placement, name=p,
+                                         original=p, value=value, type='number', pattern=float_pat),
+                                    index=['number'])
+            else:
+                # otherwise, the row can be build off of the name_maps objects:
+                # if p isn't in the index of name_maps, it probably has an s on the end:
+                pidx = p if p in name_maps.index else p[:-1]
+                # we want the start and end to stay the same but the pattern to forget the s.
+                # todo raise warning if p is still not in the index of name_maps
 
-            # handle unit plurality
-            if info.loc[pidx, 'type'] in ['volume', 'weight']:
-                # was the last number != 1?
-                is_plural = 'unknown'
-                if len(match_info) > 0:
-                    if any(match_info.type == 'number'):
-                        m = match_info.loc[match_info.type == 'number', 'value'].values
-                        if len(m) > 1:
-                            m = m[-1]
-                        else:
-                            m = m[0]
-                        # if the most recent number was 1:
-                        if m == 1:
-                            is_plural = False
-                            info['name'] = info['singular']
-                        else:
-                            is_plural = True
-                            info['name'] = info['plural']
-                        info['is_plural'] = is_plural
-                # if that didn't work:
-                if is_plural not in [True, False]:
-                    # todo raise warning that plurality could not be found...
-                    info['name'] = info['original']
+                info = pd.DataFrame(name_maps.loc[pidx, :]).T
+                info['original'] = p
+                info['start'] = start + placement
+                info['end'] = end + placement
+                info['pattern'] = pidx
+
+                # handle unit plurality
+                if info.loc[pidx, 'type'] in ['volume', 'weight']:
+                    # was the last number != 1?
+                    is_plural = 'unknown'
+                    if len(match_info) > 0:
+                        if any(match_info.type == 'number'):
+                            m = match_info.loc[match_info.type == 'number', 'value'].values
+                            if len(m) > 1:
+                                m = m[-1]
+                            else:
+                                m = m[0]
+                            # if the most recent number was 1:
+                            if m == 1:
+                                is_plural = False
+                                info['name'] = info['singular']
+                            else:
+                                is_plural = True
+                                info['name'] = info['plural']
+                            info['is_plural'] = is_plural
+                    # if that didn't work:
+                    if is_plural not in [True, False]:
+                        # todo raise warning that plurality could not be found...
+                        info['name'] = info['original']
 
             placement = info.loc[:, 'end'][0]
+            # todo change this to pd.append
             match_info = pd.concat([match_info, info])
         else:
             end = len(line)
@@ -148,7 +166,7 @@ def parse_ingredient_line(line):
 
     return match_info.fillna('').to_dict(orient='index')
 
-# todo match to numbers not followed by 'times', other numbers, 
+# todo match to numbers not followed by 'times', other numbers,
 
 
 def parse_ingredients(x):
