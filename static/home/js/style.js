a955abf153fb44384145e09ab4859d0b41e1dcc0
Hillary Sanders <hillary@premise.com>
2016-10-17 13:57:47 -0700

    collectstatic

--- static/home/js/style.js
+++ static/home/js/style.js
@@ -8,3 +8,9 @@ $(document).ready(function(){
 // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 $('.modal-trigger').leanModal();
 });
+
+
+$(document).ready(function(){
+$('.tooltipped').tooltip({delay: 50, tooltip: '<text_indent><multiplied_amount>highlighted amounts</multiplied_amount> will be multiplied</text_indent>', html: true});
+});
+
