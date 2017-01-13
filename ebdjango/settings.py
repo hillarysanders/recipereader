f95fa7d3f0cb9a8d0f833aab28815ea37c991337
Hillary Sanders <hillary@premise.com>
2017-01-12 16:36:06 -0800

    base dir in static path

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -168,8 +168,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 MEDIA_ROOT = '/Users/hills/Desktop/code/django-beanstalk/ebdjango/media/'
 # MEDIA_ROOT = '/media/'
 MEDIA_URL = '/media/'
